eabd8c494f17870b8eb9c34cc6bdf3bb729f35f9
gapon <sgapontv@gmail.com>
2022-02-01 16:28:36 +0300

    1.7 correct

--- chapter1/7.php
+++ chapter1/7.php
@@ -1,10 +1,10 @@
 <?php
-$N = 61120453;
+$N = 66120453;
 $uniqueDig = [];
 $lengthNum = 0;
 $identicalFlag = false;
 for($lengthNum; intdiv($N, 10 ** $lengthNum) > 10; $lengthNum++);
-for($i = 0; $i < $lengthNum; $i++){
+for($i = 0; $i <= $lengthNum; $i++){
     for($j = 0; $j < count($uniqueDig); $j++){
         if($uniqueDig[$j] === $N % 10){
             $identicalFlag = true;
