4334112aa5961ab3358eaecff385a99e292fb3a1
gapon <sgapontv@gmail.com>
2022-02-01 15:01:06 +0300

    1.7

---
+++ chapter1/7.php
@@ -0,0 +1,20 @@
+<?php
+$N = 61120453;
+$uniqueDig = [];
+$lengthNum = 0;
+$identicalFlag = false;
+for($lengthNum; intdiv($N, 10 ** $lengthNum) > 10; $lengthNum++);
+for($i = 0; $i < $lengthNum; $i++){
+    for($j = 0; $j < count($uniqueDig); $j++){
+        if($uniqueDig[$j] === $N % 10){
+            $identicalFlag = true;
+        }
+    }
+    $uniqueDig[] = $N % 10;
+    $N = intdiv($N, 10);
+}
+if($identicalFlag){
+    echo "number contains two identical digits";
+}else{
+    echo "number does not contain two identical digits";
+}
