5b3980e9040eb41d3493e0e239242bd9bd0f0cc1
gapon <sgapontv@gmail.com>
2022-02-03 14:14:38 +0300

    change 1.4

--- chapter1/4.php
+++ chapter1/4.php
@@ -1,34 +1,22 @@
 <?php
 
-function checkNum($num){
+for($i = 1000; $i < 10000; $i+= 2){
+    $num = $i;
     $prevDig = $num % 10;
     $num = intdiv($num, 10);
-    return (checkHigh($num, $prevDig) || checkLow($num, $prevDig));
-}
-
-function checkHigh($num, $prevDig){
+    $flagLow = true;
+    $flagHigh = true;
     while($num > 0){
         if($num % 10 >= $prevDig){
-            return false;
+            $flagLow = false;
         }
-        return checkHigh(intdiv($num, 10),$num % 10);
-    }
-    return  true;
-}
-
-function checkLow($num, $prevDig){
-    while($num > 0){
         if($num % 10 <= $prevDig){
-            return false;
+            $flagHigh = false;
         }
-        return checkLow(intdiv($num, 10),$num % 10);
+        $prevDig = $num % 10;
+        $num = intdiv($num, 10);
     }
-    return  true;
-}
-
-
-for($i = 1000; $i < 10000; $i+= 2){
-    if(checkNum($i)){
+    if($flagHigh or $flagLow){
         echo $i . "\n";
     }
 }
