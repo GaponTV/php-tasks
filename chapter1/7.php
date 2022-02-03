1ee5f08ae6bd3b27ff76167fafe2800c2646229a
gapon <sgapontv@gmail.com>
2022-02-03 15:20:52 +0300

    change 1.7

--- chapter1/7.php
+++ chapter1/7.php
@@ -1,16 +1,18 @@
 <?php
-$N = 66120453;
-$uniqueDig = [];
+$N = 12340987;
+$uniqueDig = 0;
 $lengthNum = 0;
 $identicalFlag = false;
 for($lengthNum; intdiv($N, 10 ** $lengthNum) > 9; $lengthNum++);
 for($i = 0; $i <= $lengthNum; $i++){
-    for($j = 0; $j < count($uniqueDig); $j++){
-        if($uniqueDig[$j] === $N % 10){
+    $checkNum = $uniqueDig;
+    while($checkNum > 0){
+        if($checkNum % 10 == $N % 10){
             $identicalFlag = true;
         }
+        $checkNum = intdiv($checkNum, 10);
     }
-    $uniqueDig[] = $N % 10;
+    $uniqueDig += ($N % 10) * 10 ** $i;
     $N = intdiv($N, 10);
 }
 if($identicalFlag){
